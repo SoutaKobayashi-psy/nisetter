56ad818221c7d8a5be708bc5933a51880be7e348
souta.kobayashi <soutak.work@gmail.com>
2020-06-29 00:14:36 +0000

    gravatar連携表記

--- resources/views/users/card.blade.php
+++ resources/views/users/card.blade.php
@@ -5,6 +5,10 @@
     <div class="card-body">
         {{-- ユーザのメールアドレスをもとにGravatarを取得して表示 --}}
         <img class="rounded img-fluid" src="{{ Gravatar::get($user->email, ['size' => 500]) }}" alt="">
+        <div class="mt-2">
+            <span class="mt-2">本サービスは<a href="https://ja.gravatar.com/">Gravatar</a>と連携しています</span>
+            <span class="mt-2">Gravatarで登録した画像が表示されます</span>
+        </div>
     </div>
 </div>
 {{-- フォロー／アンフォローボタン --}}
