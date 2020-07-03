44c5eed4e4bea330b2dbc1afe77e113151b13df9
souta.kobayashi <soutak.work@gmail.com>
2020-07-03 03:15:56 +0000

    投稿編集機能の実装、UI改善

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,19 +1,26 @@
 $(function() {
-	var $items = $('.dropdown-item-text');
-	for (var i = 0; i <= $items.length -1; i++) {
-		$($items[i]).click(function() {
-console.log('test');
-		});
-	}
-	///	var value = $(this);
-/*
-		var $otherText = $("#statusOtherText");
+	// 編集ボタン
+	var $items = $('.micropost-edit');
+	// 編集フォーム
+	var $microposts_form = $('.edit-content-form.own-post');
+	// 投稿内容テキスト
+	var $microposts_text = $('.show-content.own-post');
+	// 編集キャンセルボタン
+	var $cancel = $('.edit-cancel.own-post');
 
-		if (value == 3) {
-			$otherText.prop("disabled", false);
-		} else {
-			$otherText.prop("disabled", true);
-			$otherText.val("");
-		}
-*/
+	// クリックされた投稿編集フォームを表示する
+	$($items).each(function(i,ele) {
+		$(ele).click(function() {
+			$($microposts_form[i]).show();
+			$($microposts_text[i]).removeClass('d-flex').hide();
+		});
+	});
+
+	// キャンセルボタンを押下したとき
+	$($cancel).each(function(i,ele) {
+		$(ele).click(function() {
+			$($microposts_form[i]).hide();
+			$($microposts_text[i]).addClass('d-flex').show();
+		});
+	});
 });
--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -10,38 +10,40 @@
                         {!! link_to_route('users.show', $micropost->user->name, ['user' => $micropost->user->id]) !!}
                         <span class="text-muted">{{ $micropost->created_at }}</span>
                     </div>
-                    <div class="show-content d-flex justify-content-between align-items-center">
+                    <div class="show-content d-flex justify-content-between align-items-center {{ Auth::id() === $micropost->user_id ? "own-post" : "" }}">
                         {{-- 投稿内容 --}}
                         <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
-                        <div class="dropdown">
-                            <button type="button" id="dropdown1"
-                                    class="btn btn-secondary dropdown-toggle btn-sm"
-                                    data-toggle="dropdown"
-                                    aria-haspopup="true"
-                                    aria-expanded="false">
-                            オプション 
-                            </button>
-                            <div class="dropdown-menu" aria-labelledby="dropdown1">
-                                <span class="dropdown-item-text">編集する(実装中)</span>
+                        @if (Auth::id() === $micropost->user_id)
+                            <div class="dropdown">
+                                <button type="button" id="dropdown1"
+                                        class="btn btn-secondary dropdown-toggle btn-sm"
+                                        data-toggle="dropdown"
+                                        aria-haspopup="true"
+                                        aria-expanded="false">
+                                オプション 
+                                </button>
+                                <div class="dropdown-menu align-items-center" aria-labelledby="dropdown1">
+                                    <span class="dropdown-item-text micropost-edit">編集する</span>
+                                        {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
+                                            {!! Form::submit('消去する', ['class' => 'dropdown-item-text border border-0 bg-white']) !!}
+                                        {!! Form::close() !!}
+                                </div>
                             </div>
-                        </div>
+                        @endif
                     </div>
-                    <div class="edit-content-form" hidden>
+                    <div class="edit-content-form {{ Auth::id() === $micropost->user_id ? "own-post" : "" }}">
                         {{-- 投稿内容を編集する --}}
                         {!! Form::model($micropost, ['route' => ['microposts.update', $micropost->id], 'method' => 'put' ]) !!}
                             {!! Form::textarea('content', old('content'), ['class' => 'form-control']) !!}
-                        {!! Form::submit('変更を保存する', ['class' => 'btn btn-primary']) !!}
+                        <div class="d-flex">
+                            <span class="btn btn-light mr-3 edit-cancel {{ Auth::id() === $micropost->user_id ? "own-post" : "" }}">キャンセル</span>
+                            {!! Form::submit('変更を保存する', ['class' => 'btn btn-primary']) !!}
+                        </div>
                         {!! Form::close() !!}
                     </div>
                     <div class="d-flex align-items-center">
                         <i class="fas fa-heart"></i><span class="ml-1 mr-3">{{ $micropost->favorite_users_count }}</span>
                         @include('favorites.favorite_button')
-                        @if (Auth::id() === $micropost->user_id)
-                            {{-- 投稿削除ボタン --}}
-                            {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
-                                {!! Form::submit('消去する', ['class' => 'btn btn-danger btn-sm']) !!}
-                            {!! Form::close() !!}
-                        @endif
                     </div>
                 </div>
             </li>
